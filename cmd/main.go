0943fc17b7cddbcb477302c91d72a8fe9662a6bd
Skvortsovvv <vladimirs001@yandex.ru>
2022-02-04 15:54:52 +0300

    added config file, postgres files for db migration

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	todo "todo-app"
 	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializating configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while runnig http server: %s\n", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./configs")
+	return viper.ReadInConfig()
+
 }
