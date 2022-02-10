20a98ee08825c6270ebe167eac057fa1da1617e5
Vladimir Skvortsov <vladimirs001@yandex.ru>
2022-02-10 17:34:09 +0300

    edited config file, added connection to db in main func, added init of db in repository file

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 	todo "todo-app"
 	"todo-app/pkg/handler"
 	"todo-app/pkg/repository"
@@ -15,13 +17,18 @@ func main() {
 		log.Fatalf("error initializating configs: %s", err.Error())
 	}
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
 	db, err := repository.NewPostgresDB(repository.Config{
-		Host:     "localhost",
-		Port:     "5436",
-		Username: "postgres",
-		Password: "qwerty",
-		DBName:   "postgres",
-		SSLMode:  "disable",
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
 	})
 	if err != nil {
 		log.Fatalf("fail to initialize database: %s", err.Error())
