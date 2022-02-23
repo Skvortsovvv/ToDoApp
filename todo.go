04a7bc66fbdfb8a150dcc1f4ce91b9a0639aed62
Vladimir Skvortsov <vladimirs001@yandex.ru>
2022-02-23 14:06:45 +0300

    added methods to remove and update lists

--- todo.go
+++ todo.go
@@ -1,9 +1,11 @@
 package todo
 
+import "errors"
+
 type TodoList struct {
-	Id          string `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
+	Id          string `json:"id" db:"id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Description string `json:"description" db:"description"`
 }
 
 type UsersList struct {
@@ -22,4 +24,16 @@ type ListsItem struct {
 	Id     int
 	UserId int
 	ListId int
+}
+
+type UpdateListInput struct {
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+}
+
+func (i UpdateListInput) Validate() error {
+	if i.Title == nil && i.Description == nil {
+		return errors.New("update structure has no values")
+	}
+	return nil
 }
